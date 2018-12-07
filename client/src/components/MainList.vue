3c9dcddffcb2e56fea253015baa3200f8c6ce4f2
Hasegawa Yohei <yoheybrains@gmail.com>
2018-12-08 01:49:03 +0900

    added: setup client

---
+++ client/src/components/MainList.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-container>
+    <v-layout text-xs-center wrap>
+      <v-flex offset-xs2 xs8>
+        <ListPanel :title="title" :inspections="inspections" />
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    title: 'APT 検査',
+    inspections: [
+      {
+        title: '両耳分離聴検査',
+        path: 'dichoticListeningTest',
+        divider: true,
+      },
+      {
+        title: '早口音声聴取検査',
+        path: 'fastsoundListeningTest',
+        divider: true,
+      },
+      {
+        title: 'ギャップ検出閾値検査',
+        divider: true,
+      },
+      {
+        title: '雑音下単語聴取検査',
+        divider: true,
+      },
+      {
+        title: '両耳交互聴検査',
+        path: 'bothEarsAlternatingTest',
+        divider: true,
+      },
+      {
+        title: '聴覚的注意検査',
+        divider: true,
+      },
+      {
+        title: '複数音声下聴取検査',
+        path: 'multipleSoundListeningTest',
+        divider: true,
+      },
+      {
+        title: '終了',
+        divider: false,
+      },
+    ],
+  }),
+};
+</script>
+
+<style>
+.text-center{
+  text-align: center;
+}
+</style>
---
+++ client/src/components/globals/ListPanel.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-card>
+    <v-toolbar color="cyan" dark>
+      <v-toolbar-title>{{title}}</v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+
+    <v-list two-line>
+      <template v-for="(item, index) in inspections">
+
+        <v-list-tile
+          :key="item.title"
+          avatar
+          @click="link(item.path)"
+        >
+          <v-list-tile-content>
+            <v-list-tile-title v-html="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+
+        <v-divider
+          v-if="item.divider"
+          :key="index"
+        ></v-divider>
+      </template>
+    </v-list>
+</v-card>
+</template>
+
+<script>
+export default {
+  props: [
+    'title',
+    'inspections',
+  ],
+  data: () => ({
+  }),
+  methods: {
+    link(path) {
+      this.$router.push({
+        name: path,
+      });
+    },
+  },
+};
+</script>
+
+<style>
+.text-center{
+  text-align: center;
+}
+</style>
