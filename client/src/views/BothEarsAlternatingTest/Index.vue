77fea94f263745a7351dd9ca60da11f08281b404
Hasegawa Yohei <yoheybrains@gmail.com>
2018-12-08 01:50:43 +0900

    added: both ears alternating test

---
+++ client/src/views/BothEarsAlternatingTest/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <TestPanel4Column title="両耳交互聴検査" backPath="home" :tests=tests />
+</template>
+
+<script>
+import Api from '../../services/Api';
+
+export default {
+  data: () => ({
+    tests: [],
+  }),
+  async created() {
+    const result = (await Api.get('/both-ears-alternating-test')).data;
+    this.tests = result;
+  },
+}
+</script>
