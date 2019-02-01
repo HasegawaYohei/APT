9aa496906a8333be0514473f3d82fd50b30779fe
Hasegawa Yohei <yoheybrains@gmail.com>
2019-02-01 19:41:10 +0900

    added: auditory attention inspection endpoint

---
+++ server/routes/AuditoryAttentionInspection.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const config = require('../config');
+const fs = require('fs');
+
+const asyncMap = (arr, fn) => Promise.all(arr.map(async v => await fn(v)));
+
+const wrapper = fn => (req, res, next) => fn (req, res).catch(next);
+
+router.get('/', wrapper(async (req, res) => {
+  const list = fs.readdirSync(__dirname + '/../public/sound/AA');
+  const length = list.length;
+  const response = list.map((v, i) => {
+    return {
+      fullpath: `http://localhost:${config.PORT}/static/sound/AA/${v}`,
+      filename: v,
+      divider: ((i + 1) !== length),
+    };
+  });
+  return res.status(200).json(response);
+}));
+
+module.exports = router;
