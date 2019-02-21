1a9b128482b73d61d7537f808f8e392bf896f618
Hasegawa Yohei <yoheybrains@gmail.com>
2019-02-21 22:49:49 +0900

    modified: add some functions

--- client/src/services/InspectionServie.js
+++ client/src/services/InspectionServie.js
@@ -1,19 +1,102 @@
-const findUnfinishedInspection = audio => audio.statuses.some(status => status === '');
+import { remote } from 'electron';
 
-export function internalBack(audioList, cursor) {
-  const previousAudio = audioList.slice(0, cursor).reverse().find(findUnfinishedInspection);
+const findUnfinishedInspection = element => element.statuses.some(status => status === '');
+
+function getAudioFile(filepath) {
+  const fs = remote.require('fs');
+  const filenameList = fs.readdirSync(filepath);
+
+  return filenameList.filter(filename => filename.slice(-4) === '.wav').map(filename => ({
+    filename,
+    data: fs.readFileSync(`${filepath}${filename}`),
+  }));
+}
+
+export function internalBack(resultList, cursor) {
+  const previousAudio = resultList.slice(0, cursor).reverse().find(findUnfinishedInspection);
 
   if (previousAudio == null || previousAudio.cursor == null) {
-    return audioList.slice().reverse().find(findUnfinishedInspection).cursor;
+    return resultList.slice().reverse().find(findUnfinishedInspection).cursor;
   }
   return previousAudio.cursor;
 }
 
-export function internalNext(audioList, cursor) {
-  const nextAudio = audioList.slice(cursor + 1, audioList.lenght).find(findUnfinishedInspection);
+export function internalNext(resultList, cursor) {
+  const nextAudio = resultList.slice(cursor + 1, resultList.lenght).find(findUnfinishedInspection);
 
   if (nextAudio == null || nextAudio.cursor == null) {
-    return audioList.find(findUnfinishedInspection).cursor;
+    return resultList.find(findUnfinishedInspection).cursor;
   }
   return nextAudio.cursor;
+}
+
+export function generateAnswerButtonList(columnNumber) {
+  const answerButtonList = Array.from({ length: columnNumber * 2 }).map((_, i) => {
+    if (i % 2) {
+      return {
+        statusIndex: Math.floor(i / 2),
+        label: '正答',
+        color: 'success',
+        answer: true,
+      };
+    }
+
+    return {
+      statusIndex: Math.floor(i / 2),
+      label: '誤答',
+      color: 'error',
+      answer: false,
+    };
+  });
+
+  if (columnNumber === 1) return answerButtonList;
+  if (columnNumber === 2) {
+    return answerButtonList.map(answerButton => ({
+      ...answerButton,
+      label: `${answerButton.label}${(answerButton.statusIndex % 2) ? 'R' : 'L'}`,
+    }));
+  }
+  if (columnNumber === 3) {
+    return answerButtonList.map(answerButton => ({
+      ...answerButton,
+      label: `${answerButton.label}${answerButton.statusIndex + 1}`,
+    }));
+  }
+
+  return answerButtonList;
+}
+
+export function generateResultListHeader(columnNumber) {
+  if (columnNumber === 1) return ['状態'];
+  if (columnNumber === 2) {
+    return [
+      '状態L',
+      '状態R',
+    ];
+  }
+  if (columnNumber === 3) {
+    return [
+      '状態1',
+      '状態2',
+      '状態3',
+    ];
+  }
+
+  return [];
+}
+
+export function generateAudioList(audioDirPath) {
+  return getAudioFile(audioDirPath).map((audioFIle, i) => ({
+    filename: audioFIle.filename,
+    data: audioFIle.data,
+    cursor: i,
+  }));
+}
+
+export function generateResultList(audioList, columnNumber) {
+  return audioList.map((audio, i) => ({
+    filename: audio.filename,
+    statuses: Array.from({ length: columnNumber }).map(() => ''),
+    cursor: i,
+  }));
 }
