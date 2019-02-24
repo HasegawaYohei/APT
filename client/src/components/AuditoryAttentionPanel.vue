90c1053167cf9d704d279edb0b8dff04248fca9b
Hasegawa Yohei <yoheybrains@gmail.com>
2019-02-24 23:09:57 +0900

    refactored: Auditory Attention

--- client/src/components/AuditoryAttentionPanel.vue
+++ client/src/components/AuditoryAttentionPanel.vue
@@ -6,41 +6,38 @@
           <v-icon>arrow_back</v-icon>
         </v-btn>
         <v-toolbar-title>{{title}}</v-toolbar-title>
+        <v-btn
+          :large="true"
+          color="warning"
+          :style="{marginLeft: 'auto'}"
+          @click="interruption"
+          >中断終了</v-btn>
       </v-toolbar>
     </v-card>
 
     <v-layout text-xs-center mt-3 wrap>
       <v-flex xs12>
         1の後に「9」が出てきたら、スペースキーを押してください
-        <div v-if="!played">
-          <v-btn
-            class="btn-custom"
-            :large="true"
-            :disabled="!apiDone || !ready"
-            @click="play"
-            >再生</v-btn>
-        </div>
-        <div v-else>
-          <v-btn
-            :large="true"
-            class="btn-custom"
-            @click="finish"
-            >途中終了</v-btn>
-        </div>
-        <audio id="sound">
-          <source :src="sound" type="audio/wav">
-        </audio>
+        <v-btn
+          class="btn-custom"
+          :large="true"
+          :disabled="!ready"
+          @click="play"
+          >再生</v-btn>
       </v-flex>
     </v-layout>
   </div>
 </template>
 
 <script>
+import { shuffleArray, flattenArray } from '../services/ArrayService';
+import { generateAudioList, playAudio } from '../services/InspectionService';
+
 let start = 0;
 let play = false;
 let isCorrect = false;
 
-document.onkeydown = function(e) {
+document.onkeydown = function answer(e) {
   if (play === false) return;
   if (e.keyCode !== 32) return;
 
@@ -50,93 +47,75 @@ document.onkeydown = function(e) {
   if (isCorrect === true) console.log('correct!');
   else console.log('wrong...');
   console.log(time);
-}
+};
 
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function arrayShuffle(array) {
-  for (let i = array.length - 1; i >= 0; i--) {
-    let rand = Math.floor( Math.random() * ( i + 1 ) );
-    [array[i], array[rand]] = [array[rand], array[i]]
-  }
-
-  return array;
-}
-
-function flatten(array) {
-  return array.reduce((a, c) => {
-    return Array.isArray(c) ? a.concat(flatten(c)) : a.concat(c);
-  }, []);
-}
-
-function buildSoundList(inspections) {
+function buildSoundList(audioListOrigin) {
   const correctAnswer = [
     {
-      inspection: inspections[0],
+      ...audioListOrigin[0],
       correct: false,
     },
     {
-      inspection: inspections[6],
+      ...audioListOrigin[6],
       correct: true,
     },
   ];
   const correctAnswerArray = Array.from({ length: 20 }).map(() => correctAnswer);
-  const wrongAnswer1Array = Array.from({ length: 10 }).map(() => {
-    return [
-      {
-        inspection: inspections[0],
-        correct: false,
-      },
-      {
-        inspection: inspections[getRandomInt(1, 6)],
-        correct: false,
-      },
-    ];
-  });
-  const wrongAnswer2Array = Array.from({ length: 10 }).map(() => {
-    return [
-      {
-        inspection: inspections[getRandomInt(1, 6)],
-        correct: false,
-      },
-      {
-        inspection: inspections[6],
-        correct: false,
-      },
-    ];
-  });
-  const wrongAnswer3Array = Array.from({ length: 10 }).map(() => {
-    return [
-      {
-        inspection: inspections[getRandomInt(1, 6)],
-        correct: false,
-      },
-      {
-        inspection: inspections[getRandomInt(1, 6)],
-        correct: false,
-      },
-    ];
-  });
+  const wrongAnswer1Array = Array.from({ length: 10 }).map(() => ([
+    {
+      ...audioListOrigin[0],
+      correct: false,
+    },
+    {
+      ...audioListOrigin[getRandomInt(1, 6)],
+      correct: false,
+    },
+  ]));
+  const wrongAnswer2Array = Array.from({ length: 10 }).map(() => ([
+    {
+      ...audioListOrigin[getRandomInt(1, 6)],
+      correct: false,
+    },
+    {
+      ...audioListOrigin[6],
+      correct: false,
+    },
+  ]));
+  const wrongAnswer3Array = Array.from({ length: 10 }).map(() => ([
+    {
+      ...audioListOrigin[getRandomInt(1, 6)],
+      correct: false,
+    },
+    {
+      ...audioListOrigin[getRandomInt(1, 6)],
+      correct: false,
+    },
+  ]));
 
-  return flatten(arrayShuffle(correctAnswerArray.concat(wrongAnswer1Array, wrongAnswer2Array, wrongAnswer3Array)));
+  return flattenArray(
+    shuffleArray(correctAnswerArray.concat(
+      wrongAnswer1Array, wrongAnswer2Array, wrongAnswer3Array,
+    )),
+  );
 }
 
 function sleep(msec) {
   return new Promise(resolve => setTimeout(resolve, msec));
 }
 
-async function intervalFunc (callback) {
+async function intervalFunc(callback) {
   const intervals = [
     800,
     1500,
     3000,
   ];
 
-  for (let i = 0; i < 100; i++) {
-    let interval = intervals[getRandomInt(0, 3)];
-    // await Promise.all([callback(i), sleep(interval)])
+  for (let i = 0; i < 100; i += 1) {
+    const interval = intervals[getRandomInt(0, 3)];
     callback(i);
     await sleep(interval);
   }
@@ -146,12 +125,10 @@ export default {
   props: [
     'title',
     'backPath',
-    'apiDone',
-    'inspections',
+    'audioDirPath',
   ],
   data: () => ({
-    sound: '',
-    soundList: [],
+    audioList: [],
     ready: false,
     played: false,
   }),
@@ -159,14 +136,11 @@ export default {
     play() {
       play = true;
 
-      const playSound = i => {
-        const audio = document.getElementById('sound');
-        this.sound = this.soundList[i].inspection.fullpath;
-        isCorrect = this.soundList[i].correct;
-        audio.load();
-        audio.play();
+      const playSound = (i) => {
+        isCorrect = this.audioList[i].correct;
+        playAudio(this.audioList[i].buffer);
         start = performance.now();
-      }
+      };
 
       intervalFunc(playSound);
     },
@@ -174,16 +148,18 @@ export default {
       alert('Finished!!');
       this.browserBack();
     },
+    interruption() {
+    },
     browserBack() {
+      this.audioList = [];
       this.$router.push({
         name: this.backPath,
       });
     },
   },
-  updated() {
-    if (this.ready === true) return;
-
-    this.soundList = buildSoundList(this.inspections);
+  async created() {
+    const audioListOrigin = await generateAudioList(this.audioDirPath);
+    this.audioList = buildSoundList(audioListOrigin);
     this.ready = true;
   },
 };
--- client/src/views/AuditoryAttentionInspection/Index.vue
+++ client/src/views/AuditoryAttentionInspection/Index.vue
@@ -1,25 +1,20 @@
 <template>
   <AuditoryAttentionPanel
-    title="聴覚的注意検査"
-    backPath="home"
-    :apiDone=apiDone
-    :inspections=inspections />
+    :title="title"
+    :backPath="backPath"
+    :audioDirPath="audioDirPath"
+  />
 </template>
 
 <script>
-import Api from '../../services/Api';
 import AuditoryAttentionPanel from '../../components/AuditoryAttentionPanel.vue';
 
 export default {
-  data: () => ({
-    inspections: [],
-    apiDone: false,
-  }),
-  async created() {
-    const result = (await Api.get('/auditory-attention-inspection')).data;
-    this.inspections = result;
-    this.apiDone = true;
-  },
+  props: [
+    'title',
+    'backPath',
+    'audioDirPath',
+  ],
   components: { AuditoryAttentionPanel },
 };
 </script>
