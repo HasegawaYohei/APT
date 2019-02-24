262a5826956881d6e7c028ee3f0f1936f42110ab
Hasegawa Yohei <yoheybrains@gmail.com>
2019-02-24 21:16:17 +0900

    removed: unnecessary global component

--- client/src/main.js
+++ client/src/main.js
@@ -5,16 +5,12 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import ListPanel from './components/globals/ListPanel.vue';
-import InspectionPanel2Column from './components/globals/InspectionPanel2Column.vue';
-import InspectionPanel3Column from './components/globals/InspectionPanel3Column.vue';
-import InspectionPanel4Column from './components/globals/InspectionPanel4Column.vue';
+import InspectionPanel from './components/globals/InspectionPanel.vue';
 
 Vue.config.productionTip = false;
 
 Vue.component('ListPanel', ListPanel);
-Vue.component('InspectionPanel2Column', InspectionPanel2Column);
-Vue.component('InspectionPanel3Column', InspectionPanel3Column);
-Vue.component('InspectionPanel4Column', InspectionPanel4Column);
+Vue.component('InspectionPanel', InspectionPanel);
 
 sync(store, router);
 
